5da569e6c5d096cb62c8b8893bf1347ec63e595f
Scott Kirkpatrick <skirkpatrick@pandora.com>
2016-06-18 17:25:04 -0700

    Suppress cURL progress bar output

--- provision/bootstrap_node.sh
+++ provision/bootstrap_node.sh
@@ -3,11 +3,12 @@ NODE_VERSION="4.4.5"
 
 # NVM
 echo "Installing nvm"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v$NVM_VERSION/install.sh | bash > /dev/null
+curl -sSo- https://raw.githubusercontent.com/creationix/nvm/v$NVM_VERSION/install.sh | bash > /dev/null
 source /home/vagrant/.nvm/nvm.sh
 
 echo "Installing node"
-nvm install $NODE_VERSION
+# https://github.com/creationix/nvm/issues/1079
+nvm install $NODE_VERSION 2> /dev/null
 nvm alias default $NODE_VERSION
 
 # global npm installs
